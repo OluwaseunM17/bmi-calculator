4ce660e3f6aebd498322854575e464c625842c72
Oluwaseun Martins <163039472+OluwaseunM17@users.noreply.github.com>
2024-03-18 19:53:04 +0100

    Update bmi.py

--- bmi.py
+++ bmi.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
 st.title("Oluwaseun's BMI calculator")
-img=Image.open("C:\\Users\\hp\Downloads\\pexels-andres-ayrton-6551234 (1).jpg")
+img=Image.open("pexels-andres-ayrton-6551234 (1).jpg")
 st.image(img)
 st.markdown('#### what is BMI?')
 st.text("BMI is a measurement of a person\'s leanness or corpulence based on their height and weight, and is intended to quantify tissue mass.\nIt is widely used as a general indicator of whether a person has a healthy body weight for their height."
